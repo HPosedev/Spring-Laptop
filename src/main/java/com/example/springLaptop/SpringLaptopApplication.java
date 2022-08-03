6c07fffd3c8f04a763a410a5c3a4ce0ac8f1bf19
Hector <hectorpose@gmail.com>
2022-08-03 18:55:50 +0000

    initial commit

---
+++ src/main/java/com/example/springLaptop/SpringLaptopApplication.java
@@ -0,0 +1,13 @@
+package com.example.springLaptop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLaptopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringLaptopApplication.class, args);
+	}
+
+}
